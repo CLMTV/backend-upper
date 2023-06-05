a75135edb933942591eaf2c026dedb67007dbe62
Maxime Palmino <maximepalmino.pro@gmail.com>
2023-06-05 10:17:38 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+// load environment variables
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
