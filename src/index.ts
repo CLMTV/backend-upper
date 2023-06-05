c9a9ad055da01a66ddd854efb50975c57307c4a1
Maxime Palmino <maximepalmino.pro@gmail.com>
2023-06-05 13:37:27 +0200

    [ADD]Routes controllers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-// import routes from './api/routes/index';
+import routes from './api/routes/index';
 import {AppConfig} from './config/AppConfig';
 
 const app = express();
@@ -7,7 +7,7 @@ const app = express();
 // Use built-in Express middleware to parse JSON request bodies
 app.use(express.json());
 
-// app.use(routes);
+app.use(routes);
 
 app.listen(AppConfig.PORT, () => {
     console.log(`Server listening on port ${AppConfig.PORT}`);
