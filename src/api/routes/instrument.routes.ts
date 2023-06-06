ca3ec52402d07bdecc07dd033a860a9680b4739b
Maxime Palmino <maximepalmino.pro@gmail.com>
2023-06-06 12:13:37 +0200

    [ADD]Swagger + routes + controllers id

---
+++ src/api/routes/instrument.routes.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import {
+    createInstrument,
+    getInstrumentById,
+    getAllInstruments,
+    deleteInstrumentById,
+    updateInstrument
+} from "../controllers/instrument.controller";
+
+// Variables
+const router = express.Router();
+
+
+// Create a new user
+router.post("/create", createInstrument)
+
+// Get all users
+router.get('/getAll', getAllInstruments)
+
+// Get user by id
+router.get('/getById/:id', getInstrumentById)
+
+// Update category
+router.patch('/update/:id', updateInstrument)
+
+// Delete categoryById
+router.delete('/deleteById/:id', deleteInstrumentById)
+
+
+export default router;
