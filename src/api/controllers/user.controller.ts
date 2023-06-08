bcb0eed071c97b7dcad1ca8560da1c491925c081
Charles Mangin <charles.mangin@edu.univ-fcomte.fr>
2023-06-08 11:55:18 +0200

    [UPDATE]Fixing error in migration and user endpoint

--- src/api/controllers/user.controller.ts
+++ src/api/controllers/user.controller.ts
@@ -1,7 +1,6 @@
 import {Request, Response} from 'express';
 
-import {PrismaClient} from '@prisma/client';
-
+const {PrismaClient} = require('@prisma/client');
 const prisma = new PrismaClient();
 
 
