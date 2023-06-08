9e1816862c6a59fa908ee0b61d4158a030fb6b05
Charles Mangin <charles.mangin@edu.univ-fcomte.fr>
2023-06-08 10:59:25 +0200

    [UPDATE]Renaming files in order to better reflect the hierarchy

---
+++ src/api/controllers/topicMessage.controller.ts
@@ -0,0 +1,108 @@
+import {Request, Response} from 'express';
+
+const {PrismaClient} = require('@prisma/client');
+const prisma = new PrismaClient();
+
+
+const getAllUserMessages = async (req: Request, res: Response) => {
+    const {id} = req.params
+
+    if (!id) {
+        return res.status(400).json({
+            error: 'user id is required'
+        });
+    }
+    try {
+        const parsedId = parseInt(id, 10);
+        const messages = await prisma.message.findMany({
+            where: {
+                authorId: parsedId
+            },
+            include: {
+                message_reaction: true
+            }
+        });
+        res.status(200).json(messages);
+    } catch (err: any) {
+        res.status(400).json({error: err.message});
+    }
+}
+
+const getAllTopicMessages = async (req: Request, res: Response) => {
+    const {id} = req.params
+
+    if (!id) {
+        return res.status(400).json({error: 'topic id is required'});
+    }
+    try {
+        const parsedId = parseInt(id, 10);
+        const messages = await prisma.message.findMany({
+            where: {
+                topicId: parsedId
+            },
+            include: {
+                message_reaction: true
+            }
+        });
+        res.status(200).json(messages);
+    } catch (err: any) {
+        res.status(400).json({error: err.message});
+    }
+}
+
+const getMessageById = async (req: Request, res: Response) => {
+    const {id} = req.params
+
+    if (!id) {
+        return res.status(400).json({error: 'id is required'});
+    }
+    try {
+        const parsedId = parseInt(id, 10);
+        const message = await prisma.message.findUnique({
+            where: {
+                id: parsedId
+            },
+            include: {
+                message_reaction: true
+            }
+        });
+        res.status(200).json(message);
+    } catch (err: any) {
+        res.status(400).json({error: err.message});
+    }
+}
+
+const createMessage = async (req: Request, res: Response) => {
+    console.log(req.body)
+    const {content, authorId, topicId} = req.body || {};
+
+    try {
+        const message = await prisma.message.create({
+            data: {content, authorId, topicId},
+        });
+        res.status(201).json(message);
+    } catch (err: any) {
+        res.status(400).json({error: err.message});
+    }
+}
+
+const deleteMessageById = async (req: Request, res: Response) => {
+    const {id} = req.params
+
+    if (!id) {
+        return res.status(400).json({error: 'id is required'});
+    }
+    try {
+        const parsedId = parseInt(id, 10);
+        const deleteMessage = await prisma.message.delete({
+            where: {
+                id: parsedId
+            }
+        });
+        res.status(200).json(deleteMessage);
+    } catch (err: any) {
+        res.status(400).json({error: err.message});
+    }
+}
+
+export {getAllUserMessages, getAllTopicMessages, createMessage, getMessageById, deleteMessageById}
