e5302742bf87b543c3a4aa909c3fb0e24f6acf32
Lucas Huerta <huertalucas13@gmail.com>
2023-07-13 11:12:28 +0200

    [EDIT] Test link youtube video

--- src/mocks/VideoMock.ts
+++ src/mocks/VideoMock.ts
@@ -1,23 +1,30 @@
 import { Video } from "@prisma/client";
-import { faker } from '@faker-js/faker';
+import { URL } from 'url'; 
 
-export const _videoMock = (lessonIds: number[]): Video[] => {
-    const videos: Video[] = [];
-    let videoId = 1;
-  
-    for (const lessonId of lessonIds) {
-  
-      // Numéro de vidéo dans une lesson
-      const numVideo = 1;
-  
-      for (let i = 1; i <= numVideo; i++) {
-        const video: Video = {
-          id: videoId,
-          link: faker.internet.avatar(),
-        };
-        videos.push(video);
-        videoId++;
-      }
-    }
-    return videos;
+export const _videoMock = (): Video[] => {
+  const videos: Video[] = [];
+  let videoId = 1;
+
+  for (let i = 1; i <= 209; i++) {
+    // Générer le lien YouTube avec un paramètre d'index aléatoire
+    const youtubeLink = generateYoutubeLink(i);
+    
+    const video: Video = {
+      id: videoId,
+      link: youtubeLink,
+    };
+    videos.push(video);
+    videoId++;
+  }
+  return videos;
+};
+
+// Fonction pour générer un lien YouTube avec un paramètre d'index
+const generateYoutubeLink = (index: number): string => {
+  const youtubeLink = 'https://www.youtube.com/watch?v=EsqrV68pHLc&list=PLyVrZcjexmyOVJyVZyWGuip-kuJf7ciqx';
+
+  const url = new URL(youtubeLink);
+  url.searchParams.set('index', index.toString());
+
+  return url.toString();
 };
--- src/utils/fetchMock/createVideo.ts
+++ src/utils/fetchMock/createVideo.ts
@@ -5,18 +5,18 @@ import { fetchWrapper } from "../fetch-wrapper";
 
 const courseMock = _courseMock();
 const lessonMock = _lessonMock(courseMock.map(course => course.id));
-const videoMock = _videoMock(lessonMock.map(course => course.id));
+const videoMock = _videoMock();
 
 const url = `http://localhost:4000/video/create`;
 
 export const createVideo = async() => {
-    for (let i = 0; i < videoMock.length; i++) {
-        var raw = JSON.stringify(videoMock[i]);
-        const config = {
-            method: 'POST', 
-            body: raw
-        }
-    
-        await fetchWrapper(url, config);
-    }
-}
+  for (let i = 0; i < videoMock.length; i++) {
+    const raw = JSON.stringify(videoMock[i]);
+    const config = {
+      method: 'POST',
+      body: raw
+    };
+
+    await fetchWrapper(url, config);
+  }
+};
