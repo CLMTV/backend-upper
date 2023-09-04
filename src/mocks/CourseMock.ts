50d1a28ff0ac9dc83d595fc4e5ea7fa31e15053b
Lucas Huerta <huertalucas13@gmail.com>
2023-09-04 13:40:19 +0200

    [EDIT] Mocks courses avec schéma modifié

--- src/mocks/CourseMock.ts
+++ src/mocks/CourseMock.ts
@@ -3,6 +3,7 @@ import { faker } from '@faker-js/faker';
 
 export const _courseMock = (): Course[] => {
     const courses: Course[] = [];
+    const idPlan = faker.number.int({ min: 1, max: 3 });
   
     for (let i = 1; i <= 30; i++) {
       const course: Course = {
@@ -10,7 +11,10 @@ export const _courseMock = (): Course[] => {
         name: faker.music.songName(),
         description: faker.lorem.paragraph(),
         createdAt: faker.date.anytime(),
-        updatedAt: faker.date.anytime()
+        updatedAt: faker.date.anytime(), 
+        planId: idPlan,
+        mediaId: 1, // id test
+        authorId: 1 // id test
       };
       courses.push(course);
     }
